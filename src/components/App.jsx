aca4aa106c858e02a05c7dc04f4dda312338d040
Jamie Bryson <jamieab98@gmail.com>
2025-10-07 18:49:40 -0400

    imported About.jsx to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import blogData from "../data/blog";
+import About from "./About.jsx";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      Testing
+      {About()}
     </div>
   );
 }
